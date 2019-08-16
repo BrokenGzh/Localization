441510dd4ba3c43deb81d86c0ce11ad84978527e
BrokenGzh <37162264+BrokenGzh@users.noreply.github.com>
2019-08-16 11:06:28 +0800

    update

--- FirstVideoObjectDetection.py
+++ FirstVideoObjectDetection.py
@@ -8,22 +8,31 @@ Created on Thu Aug 15 15:00:34 2019
 # coding: utf-8
 from imageai.Detection import VideoObjectDetection
 import os
+
+def forFrame(frame_number, output_array, output_count):
+    print("FOR FRAME " , frame_number)
+    print("Output for each object : ", output_array)
+    print("Output count for unique objects : ", output_count)
+    print("------------END OF A FRAME --------------")
  
 execution_path=os.getcwd()  
- 
+
 detector = VideoObjectDetection()
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath(
     os.path.join(execution_path,'resnet50_coco_best_v2.0.1.h5'))
-detector.loadModel()
+detector.loadModel(detection_speed="fast")
+
+custom_objects = detector.CustomObjects(person=True, bicycle=True, motorcycle=True)
 
 custom_objects = detector.CustomObjects(
     person=True, bicycle=True, motorcycle=True) 
 
-video_path = detector.detectObjectsFromVideo(
+video_path = detector.detectCustomObjectsFromVideo(
+    custom_objects=custom_objects,
     input_file_path=os.path.join(execution_path, 'traffic.mp4'),
-    output_file_path=os.path.join(execution_path, 'videos',
-                                  'traffic-detected'),
+    output_file_path=os.path.join(execution_path,  'traffic-detected'),
     frames_per_second=20,
-    log_progress=True)
-print(video_path)
+    frame_detection_interval=5,
+    per_frame_function=forFrame,
+    minimum_percentage_probability=30)
